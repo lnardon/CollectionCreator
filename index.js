2a4227353a4e436c1da4bf7d2f0d650494562c6b
lnardon <lucaslnardon@gmail.com>
2021-03-10 01:09:06 -0300

    ✨ Add remaining files for the extension

---
+++ index.js
@@ -0,0 +1,16 @@
+function handleClick() {
+  const theme = {
+    setPageBGColor: getInputValue("setPageBGColor"),
+    setAllFontsColors: getInputValue("setAllFontsColors"),
+    setIconsColor: getInputValue("setIconsColor"),
+  };
+
+  localStorage.setItem("notionTheme", JSON.stringify(theme));
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, theme);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("addBtn").addEventListener("click", handleClick);
+});
