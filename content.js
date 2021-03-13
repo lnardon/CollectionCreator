b2cafcc0b98960e5a50b61bd90d322de357036ec
lnardon <lucaslnardon@gmail.com>
2021-03-12 22:34:50 -0300

    ✨ Add source link logger

--- content.js
+++ content.js
@@ -1,5 +1,18 @@
 function gotMessage() {
   console.log("Got");
 }
+window.onload = () => {
+  document.addEventListener("click", (e) => {
+    console.log(e.target.src);
+  });
+  let auxDiv = document.createElement("div");
+  auxDiv.style.position = "absolute";
+  auxDiv.style.width = "1000px";
+  auxDiv.style.height = "1000px";
+  auxDiv.style.backgroundColor = "#313131";
+  auxDiv.style.zIndex = "1000000";
+  auxDiv.style.top = "0px";
+  document.body.prepend(auxDiv);
+};
 
 chrome.runtime.onMessage.addListener(gotMessage);
