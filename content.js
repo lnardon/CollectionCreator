e280f87851e3430bdce73b2641a1d85c2722c583
lnardon <lucaslnardon@gmail.com>
2021-03-13 22:20:06 -0300

    ✨ Add copy to clipboard img src link

--- content.js
+++ content.js
@@ -12,9 +12,28 @@ function copyToClipboard(text) {
   return result;
 }
 
+function getElements(e) {
+  const x = e.clientX;
+  const y = e.clientY;
+  let stack = [];
+  let foundImage = false;
+  let hoveredElement = document.elementFromPoint(x, y);
+
+  stack.push(hoveredElement);
+  while (!foundImage) {
+    if (hoveredElement.tagName !== "IMG") {
+      hoveredElement.style.pointerEvents = "none";
+      hoveredElement = document.elementFromPoint(x, y);
+    } else {
+      copyToClipboard(hoveredElement.src);
+      foundImage = !foundImage;
+    }
+  }
+}
+
 window.onload = () => {
   document.addEventListener("click", (e) => {
-    copyToClipboard(e.target.src);
+    getElements(e);
   });
 };
 
