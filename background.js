b3f49422498e009d4f961715d84ccc31dbfceac5
lnardon <lucaslnardon@gmail.com>
2021-04-22 19:52:29 -0300

    ✨ Add copy to clipboard functionality

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onConnect.addListener((externalPort) => {
   externalPort.onDisconnect.addListener(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { type: "CLOSE_EXTENSION" });
-    });
+    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   chrome.tabs.sendMessage(tabs[0].id, { type: "CLOSE_EXTENSION" });
+    // });
   });
 });
--- content.js
+++ content.js
@@ -5,55 +5,24 @@ function copyToClipboard(text) {
   input.select();
   const result = document.execCommand("copy");
   document.body.removeChild(input);
-  return result;
-}
-
-function getImageUrl(e) {
-  const x = e.clientX;
-  const y = e.clientY;
-  let stack = [];
-  let foundImage = false;
-  let hoveredElement = document.elementFromPoint(x, y);
-
-  stack.push(hoveredElement);
-  while (!foundImage) {
-    if (hoveredElement.tagName !== "IMG") {
-      hoveredElement.style.pointerEvents = "none";
-      stack.push(hoveredElement);
-      hoveredElement = document.elementFromPoint(x, y);
-    } else {
-      copyToClipboard(hoveredElement.src);
-      undoPointerEvents(stack);
-      removeListener();
-      foundImage = !foundImage;
-      alert("Link copied to clipboard!");
-    }
-  }
-}
-
-function undoPointerEvents(elements) {
-  elements.forEach((el) => {
-    el.style.pointerEvents = "auto";
+  document.removeEventListener("click", (e) => {
+    getImageUrl(e);
   });
 }
 
-function removeListener() {
-  document.removeEventListener("click", getImageUrl);
+function getImageUrl(element) {
+  if (element.src) {
+    text = element.src;
+  } else {
+    text = element.srcset;
+  }
+  console.log(element.target.srcset || element.target.src);
+  copyToClipboard(element.target.srcset || element.target.src);
 }
 
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message) {
-  console.log(message);
-  switch (message.type) {
-    case "GET_LINK":
-      document.addEventListener("click", (e) => {
-        getImageUrl(e);
-      });
-      break;
-    // case "CLOSE_EXTENSION":
-    //   alert("SKRT");
-    //   break;
-    default:
-      break;
-  }
+  document.addEventListener("click", (e) => {
+    getImageUrl(e);
+  });
 }
