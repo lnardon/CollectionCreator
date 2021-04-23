c41d70048424beaf2a4adea4daacbbd0779e8fc6
lnardon <lucaslnardon@gmail.com>
2021-04-23 00:06:12 -0300

    ✨ Add source code link

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const port = chrome.runtime.connect();
 chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
   chrome.tabs.sendMessage(tabs[0].id, { type: "GET_LINK" });
 });
+
+document
+  .getElementsByClassName("link")[0]
+  .addEventListener("click", () =>
+    window.open(`https://github.com/lnardon/CollectionCreator`)
+  );
